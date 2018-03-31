6a896b54f9b2655978d05b369e30cccb7992d013
patrick <patmagauran.j@gmail.com>
2018-03-31 11:39:35 -0400

    Added some reception verification. Need to work on latency

--- mycroft/audio/UDPAudio.py
+++ mycroft/audio/UDPAudio.py
@@ -1,4 +1,7 @@
+import select
 import socket
+import time
+
 from mycroft.util.log import LOG
 
 clients = []
@@ -7,20 +10,49 @@ clients = []
 def playAudio(audio):
     LOG.info('Trying to play UDP audio')
     LOG.info(str(clients))
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # LOG.info(str(audio))
     try:
-        udp.setblocking(0)
-        LOG.info("Connection Status" + str(udp.connect_ex(("192.168.1.5", 50005))))
-        totalsent = 0
-        udp.send("START")
-        udp.send(str(audio.samplerate))
-        udp.send(str(audio.channels))
+        LOG.info("Connection Status" + str(udp_socket.connect_ex(("192.168.1.5", 50006))))
+        udp_socket.setblocking(0)
+        send = True
+
+        retries = 0
+        while send and retries < 10:
+            readable, writable, exceptional = select.select(
+                [udp_socket], [udp_socket], [udp_socket])
+            for s in writable:
+                s.send("START{SR: " + str(audio.samplerate) + ",CH: " + str(audio.channels) + "}")
+            for s in readable:
+                data = s.recv(4096)
+                if data:
+                    if "STARTING" in str(data):
+                        send = False
+            time.sleep(.1)
+            retries += 1
+        if retries == 10:
+            return
+        udp_socket.setblocking(1)
         for buf in audio:
-            udp.send(buf)
-        udp.send("END")
-        udp.close()
+            udp_socket.sendall(buf)
+        udp_socket.setblocking(0)
+        send = True
+        retries = 0
+        # while send and retries < 10:
+        #     readable, writable, exceptional = select.select(
+        #         [udp_socket], [udp_socket], [udp_socket])
+        #     for s in writable:
+        #         s.send("END")
+        #     for s in readable:
+        #         data = s.recv(4096)
+        #         if data:
+        #             if "ENDING" in str(data):
+        #                 send = False
+        #     time.sleep(.3)
+        #     retries += 1
+
+        udp_socket.close()
     except socket.error as msg:
-        udp.close()
+        udp_socket.close()
         LOG.error(msg)
 
